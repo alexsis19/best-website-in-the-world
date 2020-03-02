e5721c1c09aa5029043c831e37fc10462c2928fa
Sis <sis.maharjan@lisnepal.com.np>
2020-03-02 14:13:31 +0545

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "about", component: AboutComponent, pathMatch: "full" },
+  { path: "contact", component: ContactComponent, pathMatch: "full" },
+  { path: "projects", component: ProjectsComponent, pathMatch: "full" },
+  { path: "resume", component: ResumeComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
